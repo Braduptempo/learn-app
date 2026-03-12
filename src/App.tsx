b3bd3f6570fa7894f13dca14181180b3614903e4
Ryan <s1192232@windesheim.nl>
2026-03-12 17:44:43 +0100

    second commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <div className='menu' >
+      <h1>Leer App</h1>
+
+      <div className='menu-buttons'>
+        <button>Overzicht</button>
+        <button>Vakken</button>
+        <button>Resultaten</button>
+      </div>
+    </div> 
+    </>
+  )
+}
+
+export default App
